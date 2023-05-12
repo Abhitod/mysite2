fe395dbf9f6240eca03aa88d714c845f3a6a8660
Abhitod <118940064+Abhitod@users.noreply.github.com>
2023-05-13 00:48:38 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,24 +1,10 @@
-var express = require('express');
-var path=require('path');
-var app = express();
+import express from "express"
+let app=express()
 
-//server configuration
-app.use(express.static(path.join(__dirname,'public')));
+app.get("/",(req,res)=>{
+    res.send("running on server")
+})
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(___dirname + '/index.html'));
- });
-
-
-app.get('/hello', function (req, res) {
-    var person={firstName:'Aniket',lastName:'Bali',age:47};
-    res.send(person);
-});
-
-var server = app.listen(7667, function () {
-    var host = server.address().address
-    var port = server.address().port
-    console.log("Example app listening at http://localhost:7667", host, port)
-});
-
-  
+app.listen(1760,()=>{
+    console.log("running on port 1760")
+})
